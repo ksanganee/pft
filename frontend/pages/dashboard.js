2b3109c570d29a4dc29bd3f3090fd9eeee496553
Kiran <sanganee12@gmail.com>
2022-12-15 19:50:20 +0000

    fixed console errors on login, added signout button

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,27 +1,30 @@
-import PocketBase from 'pocketbase';
-import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react';
+import PocketBase from "pocketbase";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import SignoutButton from "../components/SignoutButton";
 
 export default function Dashboard() {
-	let router = useRouter()
+	let router = useRouter();
 
-	const client = new PocketBase('http://127.0.0.1:8090');
-	const [userModel, setUserModel] = useState(null)
+	const client = new PocketBase("http://127.0.0.1:8090");
+
+	const [userModel, setUserModel] = useState(null);
 
 	useEffect(() => {
 		if (client.authStore.model == null) {
 			router.push({
-				pathname: '/login',
-				query: { name: 'You must login first' }
-			})
+				pathname: "/login",
+				query: { name: "You must login first" },
+			});
 		} else {
-			setUserModel(client.authStore.model)
+			setUserModel(client.authStore.model);
 		}
-	}, [client.authStore.model, router]);
+	}, []);
 
 	return (
 		<div className="flex items-center justify-center h-screen">
 			{userModel && <p>Signed in as {userModel.email}</p>}
+			<SignoutButton />
 		</div>
-	)
+	);
 }
--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,38 +1,47 @@
-import { useRouter } from 'next/router'
-import PocketBase from 'pocketbase';
-import { useState, useEffect } from 'react';
+import { useRouter } from "next/router";
+import PocketBase from "pocketbase";
+import { useState, useEffect } from "react";
+import Link from 'next/link'
 
 export default function Login() {
-	let router = useRouter()
+	let router = useRouter();
 
-	const client = new PocketBase('http://127.0.0.1:8090');
+	const client = new PocketBase("http://127.0.0.1:8090");
 
-	const [email, setEmail] = useState("")
-	const [password, setPassword] = useState("")
-	const [error, setError] = useState("")
-	const [query, setQuery] = useState("")
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const [query, setQuery] = useState("");
 
 	useEffect(() => {
 		setQuery(router.query.name);
 	}, [router.query]);
 
 	const handleLogin = async (e) => {
-		e.preventDefault()
-		await client.users.authViaEmail(
-			email,
-			password
-		).then((_) => {
-			router.push("/dashboard")
-		}).catch((err) => {
-			setQuery("");
-			setError("Login failed")
-		});
-	}
+		e.preventDefault();
+		await client.collections("users")
+			.authWithPassword(email, password)
+			.then((_) => {
+				router.push("/dashboard");
+			})
+			.catch((err) => {
+				console.log(err);
+				setQuery("");
+				setError("Login failed");
+			});
+	};
 
 	return (
 		<div className="flex items-center justify-center h-screen">
-			<form className="flex flex-col min-w-max formFixedWidth"onSubmit={handleLogin}>
-				{query && <p className="text-green-500 bg-green-200 p-2 rounded-lg mb-2 text-center">{query}</p>}
+			<form
+				className="flex flex-col min-w-max formFixedWidth"
+				onSubmit={handleLogin}
+			>
+				{query && (
+					<p className="text-green-500 bg-green-200 p-2 rounded-lg mb-2 text-center">
+						{query}
+					</p>
+				)}
 				<input
 					type="email"
 					className="rounded-lg border-gray-300 border-2 p-1.5 mb-2"
@@ -45,10 +54,17 @@ export default function Login() {
 					placeholder="Password"
 					onChange={(e) => setPassword(e.target.value)}
 				/>
-				{error && <p className="text-red-500 bg-red-200 p-2 rounded-lg mb-2 text-center">{error}</p>}
+				{error && (
+					<p className="text-red-500 bg-red-200 p-2 rounded-lg mb-2 text-center">
+						{error}
+					</p>
+				)}
 				<button className="rounded-lg bg-[#fb923c] p-1.5 right-1 text-white">
 					Login
 				</button>
+				<div className="flex items-center justify-center">
+					<Link className="p-2" href="/signup">Don't have an account?</Link>
+				</div>
 			</form>
 		</div>
 	);
