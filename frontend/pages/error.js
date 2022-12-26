a5699c2309c1341c9866022ed0c7a12be545488a
Kiran <sanganee12@gmail.com>
2022-12-26 16:47:03 +0000

    fiddled with eslint, think did nothing

--- frontend/pages/error.js
+++ frontend/pages/error.js
@@ -9,10 +9,7 @@ export default function error() {
 				<p className="text-red-500 bg-red-200 p-2 rounded mb-4 text-center">
 					Something went wrong
 				</p>
-				<VisitLink
-					destination="/dashboard"
-					text="Dashboard"
-				/>
+				<VisitLink destination="/dashboard" text="Dashboard" />
 			</VStack>
 		</Centred>
 	);
