b0c856790591204d0291f8aa0b345b369636464d
Kiran <sanganee12@gmail.com>
2022-12-27 17:25:37 +0000

    moved to separate link component

---
+++ frontend/components/PlaidLinkButtons.jsx
@@ -0,0 +1,70 @@
+import { useState, useCallback } from "react";
+import { usePlaidLink } from "react-plaid-link";
+
+export default function Dashboard(props) {
+	const [token, setToken] = useState("");
+
+	const createLinkToken = async () => {
+		await fetch("/api/create_link_token", {
+			method: "POST",
+			body: JSON.stringify({
+				userId: props.userModel.id,
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setToken(data.link_token);
+			});
+	};
+
+	const linkSuccessCallback = useCallback(async (publicToken, _) => {
+		await fetch("/api/send_public_token", {
+			method: "POST",
+			body: JSON.stringify({
+				publicToken,
+				userId: props.userModel.id,
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+			});
+	}, [props.userModel]);
+
+	const linkErrorCallback = useCallback((_) => {
+		router.push("/error");
+	}, []);
+
+	const { open, ready } = usePlaidLink({
+		token: token,
+		onSuccess: linkSuccessCallback,
+		onExit: linkErrorCallback,
+	});
+
+	return (
+		<>
+			<button
+				className="rounded bg-[#fb923c] p-2 right-1 text-white mb-1"
+				onClick={createLinkToken}
+			>
+				Start Process
+			</button>
+
+			{token && (
+				<>
+					<p className="mb-1 mt-1">{token}</p>
+					{ready && (
+						<button
+							className="rounded bg-[#fb923c] p-2 right-1 text-white mb-1 mt-1"
+							onClick={() => {
+								open();
+							}}
+						>
+							Open Link
+						</button>
+					)}
+				</>
+			)}
+		</>
+	);
+}
--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,15 +1,10 @@
 import PocketBase from "pocketbase";
 import { useRouter } from "next/router";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState } from "react";
 import Centered from "../layouts/Centred";
 import VStack from "../layouts/VStack";
 import LogoutButton from "../components/LogoutButton";
-import {
-	usePlaidLink,
-	PlaidLinkOnEvent,
-	PlaidLinkOnExit,
-	PlaidLinkOptions,
-} from "react-plaid-link";
+import PlaidLinkButtons from "../components/PlaidLinkButtons";
 
 export default function Dashboard() {
 	let router = useRouter();
@@ -17,20 +12,6 @@ export default function Dashboard() {
 	const pb = new PocketBase("http://127.0.0.1:8090");
 
 	const [userModel, setUserModel] = useState(null);
-	const [token, setToken] = useState(null);
-
-	const createLinkToken = async (userId) => {
-		await fetch("/api/create_link_token", {
-			method: "POST",
-			body: JSON.stringify({
-				userId,
-			}),
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				setToken(data.link_token);
-			});
-	};
 
 	useEffect(() => {
 		if (pb.authStore.model == null) {
@@ -40,65 +21,14 @@ export default function Dashboard() {
 			});
 		} else {
 			setUserModel(pb.authStore.model);
-		}
+		}	
 	}, []);
-
-	const startProcess = async () => {
-		createLinkToken(pb.authStore.model.id);
-	};
-
-	const onSuccess = useCallback(async (publicToken, metadata) => {
-		await fetch("/api/send_public_token", {
-			method: "POST",
-			body: JSON.stringify({
-				publicToken,
-				userId: pb.authStore.model.id,
-			}),
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				console.log(data);
-			});
-		console.log(publicToken, metadata);
-	}, []);
-
-	const onExit = useCallback((error, metadata) => {
-		router.push("/error");
-	}, []);
-
-	const { open, ready } = usePlaidLink({
-		token,
-		onSuccess,
-		onExit,
-	});
 
 	return (
 		<Centered>
 			<VStack>
 				{userModel && <p>Signed in as {userModel.email}</p>}
-
-				<button
-					className="rounded bg-[#fb923c] p-2 right-1 text-white mb-1 mt-1"
-					onClick={startProcess}
-				>
-					Start Process
-				</button>
-
-				{token && (
-					<>
-						<p className="mb-2">{token}</p>
-						{ready && (
-							<button
-								className="rounded bg-[#fb923c] p-2 right-1 text-white mb-1 mt-1"
-								onClick={() => {
-									open();
-								}}
-							>
-								Open Link
-							</button>
-						)}
-					</>
-				)}
+				<PlaidLinkButtons userModel={userModel}/>
 				<LogoutButton />
 			</VStack>
 		</Centered>
