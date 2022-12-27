739dd8087a2b853b5101a8f5048d4863ee8eed56
Kiran <sanganee12@gmail.com>
2022-12-27 17:25:37 +0000

    moved to separate link component

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,10 +1,22 @@
+import { useRouter } from "next/router";
+import PocketBase from "pocketbase";
 import LoginForm from "../components/LoginForm";
 import Centred from "../layouts/Centred";
+import { useEffect } from "react";
 
 export default function LoginPageHandler() {
+	let router = useRouter();
+
+	useEffect(() => {
+		const pb = new PocketBase("http://127.0.0.1:8090");
+		if (pb.authStore.model != null) {
+			router.push("/dashboard");
+		}
+	}, []);
+
 	return (
 		<Centred>
 			<LoginForm />
 		</Centred>
-	)
+	);
 }
