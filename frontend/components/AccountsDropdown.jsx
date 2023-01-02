567e6360808d8aa11b34326cbe87e80f6e19fe94
Kiran <sanganee12@gmail.com>
2023-01-02 20:25:43 +0000

    clearer transactions, better accounts and tabs

---
+++ frontend/components/AccountsDropdown.jsx
@@ -0,0 +1,110 @@
+import { useState, useEffect } from "react";
+import PlaidLinkButtons from "./PlaidLinkButtons";
+import LogoutButton from "./LogoutButton";
+
+export default function AccountsDropdown(props) {
+	const [accounts, setAccounts] = useState([]);
+	const [dropped, setDropped] = useState(false);
+
+	const getAccounts = async () => {
+		await fetch("/api/get_accounts", {
+			method: "POST",
+			body: JSON.stringify({
+				userId: props.userModel.id,
+			}),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				setAccounts(data.accounts);
+				props.setActiveAccounts(data.accounts);
+			});
+	};
+
+	useEffect(() => {
+		getAccounts();
+	}, []);
+
+	useEffect(() => {
+		const checkboxes = document.querySelectorAll("input[type=checkbox]");
+		checkboxes.forEach((checkbox) => {
+			checkbox.checked = true;
+		});
+	}, [accounts]);
+
+	return (
+		<>
+			<button
+				id="dropdownBgHoverButton"
+				className="rounded bg-[#fb923c] p-2 text-white hover:bg-[#fb923c]/80 mt-2"
+				type="button"
+				onClick={() => setDropped(!dropped)}
+			>
+				<div className="flex flex-row justify-between">
+					Accounts
+					<svg
+						className={`w-4 h-4 mt-[5px] ml-1 ${
+							dropped ? "" : "-rotate-90"
+						} duration-75`}
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
+					</svg>
+				</div>
+			</button>
+
+			<div
+				className={`${
+					dropped ? "" : "hidden"
+				} z-10 bg-white rounded shadow absolute mt-2`}
+			>
+				<ul
+					className="p-3 space-y-1 text-sm text-gray-700"
+					aria-labelledby="dropdownBgHoverButton"
+				>
+					{accounts.map((account, i) => {
+						return (
+							<li key={i}>
+								<div className="flex p-2 rounded hover:bg-gray-100">
+									<label className="inline-flex relative items-center w-full cursor-pointer">
+										<input
+											type="checkbox"
+											className="peer sr-only"
+											onChange={(e) => {
+												if (e.target.checked) {
+													props.setActiveAccounts([
+														...props.activeAccounts,
+														account,
+													]);
+												} else {
+													props.setActiveAccounts(
+														props.activeAccounts.filter(
+															(activeAccount) =>
+																activeAccount.account_id !=
+																account.account_id
+														)
+													);
+												}
+											}}
+										/>
+										<div className="w-9 h-5 bg-gray-200 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-[#fb923c]"></div>
+										<span className="ml-3">
+											{account.name} in{" "}
+											{account.institution}
+										</span>
+									</label>
+								</div>
+							</li>
+						);
+					})}
+					<PlaidLinkButtons
+						userModel={props.userModel}
+						refresh={getAccounts}
+					/>
+					<LogoutButton />
+				</ul>
+			</div>
+		</>
+	);
+}
--- frontend/components/LogoutButton.jsx
+++ frontend/components/LogoutButton.jsx
@@ -16,11 +16,41 @@ export default function LogoutButton() {
 	};
 
 	return (
-		<button
-			className="rounded bg-[#fb923c] pt-1 pb-1 pl-2 pr-2 right-1 text-white"
-			onClick={handleSignout}
-		>
-			Logout
-		</button>
+		<li>
+			<div
+				// className="flex p-2 rounded hover:bg-[#fb923c]/80 h-[36px] items-center h-screen bg-[#fb923c] text-white"
+				className="flex p-2 rounded hover:bg-gray-100 h-[36px] items-center h-screen"
+				onClick={handleSignout}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 26"
+					className="w-[36px] h-[20px] mr-1 ml-1 pr-[1px]"
+					fill="#FB923C"
+					// make the stroke extra rounded
+					stroke-linecap="round"
+					// fill="#FFFFFF"
+				>
+					<path d="M15,24H0V2h15v8h-2V4H2v18h11v-6h2V24z M18.4,18.7L17,17.3l3.3-3.3H5v-2h15.3L17,8.7l1.4-1.4L24,13L18.4,18.7z"></path>{" "}
+				</svg>
+				{/* <svg
+					viewBox="-2 -2 24.00 24.00"
+					xmlns="http://www.w3.org/2000/svg"
+					className="w-[36px] h-[20px] mr-2"
+					fill="#FB923C"
+				>
+					<path
+						d="M9 9V1C9 0.44772 9.4477 0 10 0C10.5523 0 11 0.44772 11 1V9H19C19.5523 9 20 9.4477 20 10C20 10.5523 19.5523 11 19 11H11V19C11 19.5523 10.5523 20 10 20C9.4477 20 9 19.5523 9 19V11H1C0.44772 11 0 10.5523 0 10C0 9.4477 0.44772 9 1 9H9z"
+					></path>
+				</svg> */}
+				<p className="ml-[5px] mr-8">Logout</p>
+			</div>
+		</li>
+		// <button
+		// 	className="rounded bg-[#fb923c] pt-1 pb-1 pl-2 pr-2 right-1 text-white"
+		// 	onClick={handleSignout}
+		// >
+		// 	Logout
+		// </button>
 	);
 }
