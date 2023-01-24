3257718c2da93f65e0ce87b3ef022cc5d043a305
Kiran <sanganee12@gmail.com>
2023-01-24 15:56:04 +0000

    removed centre component

--- frontend/pages/error.js
+++ frontend/pages/error.js
@@ -1,16 +1,15 @@
 import VisitLink from "../components/VisitLink";
-import Centred from "../layouts/Centred";
 import VStack from "../layouts/VStack";
 
 export default function ErrorHandler() {
 	return (
-		<Centred>
+		<div className="flex justify-center items-center h-screen">
 			<VStack>
 				<p className="text-red-500 bg-red-200 p-2 rounded mb-4 text-center">
 					Something went wrong
 				</p>
 				<VisitLink destination="/dashboard" text="Dashboard" />
 			</VStack>
-		</Centred>
+		</div>
 	);
 }
